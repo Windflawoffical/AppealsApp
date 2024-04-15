89076f0504b5c8beab133eac6e577608c2dc129b
Kirill "Windflaw" Valikov <93864873+Windflawoffical@users.noreply.github.com>
2024-04-15 16:17:56 +0300

    Test Ktor client (Work correctly)

--- app/src/main/java/com/example/appealsapp/MainActivity.kt
+++ app/src/main/java/com/example/appealsapp/MainActivity.kt
@@ -3,41 +3,52 @@ package com.example.appealsapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.produceState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.appealsapp.features.feature_appeal.data.remote.AppealService
+import com.example.appealsapp.features.feature_appeal.data.remote.dto.AppealResponse
+import com.example.appealsapp.features.feature_user.data.remote.UserService
 import com.example.appealsapp.ui.theme.AppealsAppTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val userClient = UserService.create()
+    private val appealClient = AppealService.create()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val posts = produceState<List<AppealResponse>>(initialValue = emptyList(), producer = {value = appealClient.getAppeals()})
             AppealsAppTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                Surface(color = MaterialTheme.colorScheme.background)
+                {
+                    LazyColumn {
+                        items(posts.value) {
+                            Column(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(16.dp)
+                            ) {
+                                Text(text = it.description, fontSize = 20.sp)
+                                Spacer(modifier = Modifier.height(4.dp))
+                            }
+                        }
+                    }
                 }
             }
         }
     }
 }
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-//@Preview(showBackground = true)
-//@Composable
-//fun GreetingPreview() {
-//    AppealsAppTheme {
-//        Greeting("Android")
-//    }
-//}
