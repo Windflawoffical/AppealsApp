0e579215d575020dc930e26a7f54d59d6a4d721d
Kirill "Windflaw" Valikov <93864873+Windflawoffical@users.noreply.github.com>
2024-04-17 16:04:52 +0300

    FloatingActionButton for add appeal

--- app/src/main/java/com/example/appealsapp/features/feature_appeal/presentation/appeals/AppealsScreen.kt
+++ app/src/main/java/com/example/appealsapp/features/feature_appeal/presentation/appeals/AppealsScreen.kt
@@ -12,11 +12,16 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -37,41 +42,27 @@ fun AppealsScreen(
     val state = viewModel.state.value
 
     state.appeals?.let {
-        LazyColumn {
-            items(state.appeals) {
-                Card(
-                    onClick = { navController.navigate(Screen.AddEditAppealScreen.route) },
-                    modifier = Modifier.padding(horizontal = 12.dp, vertical = 12.dp).fillMaxWidth(),
-                ) {
-                    Text(text = it.description, fontSize = 20.sp)
-                    Spacer(modifier = Modifier.height(4.dp))
-                    Text(text = it.user.nickname, fontSize = 14.sp)
-
+        Column {
+            LazyColumn {
+                items(state.appeals) {
+                    Card(
+                        onClick = { navController.navigate(Screen.AddEditAppealScreen.route) },
+                        modifier = Modifier
+                            .padding(horizontal = 12.dp, vertical = 12.dp)
+                            .fillMaxWidth(),
+                    ) {
+                        Text(text = it.description, fontSize = 20.sp)
+                        Spacer(modifier = Modifier.height(4.dp))
+                        Text(text = it.user.nickname, fontSize = 14.sp)
+                    }
                 }
+            }
+            FloatingActionButton(
+                onClick = { navController.navigate(Screen.AddEditAppealScreen.route) },
+                modifier = Modifier.padding(horizontal = 12.dp, vertical = 12.dp).align(Alignment.End))
+            {
+                Icon(Icons.Default.Add, "Add")
             }
         }
     }
-
-
-//    Card(
-//        modifier = Modifier.padding(horizontal = 8.dp, vertical = 8.dp).fillMaxWidth(),
-//        shape = RoundedCornerShape(corner = CornerSize(16.dp)),
-//    ) {
-//        Row {
-//            state.appeals?.let {
-//                LazyColumn {
-//                    items(state.appeals) {
-//                        Column(
-//                            modifier = Modifier.fillMaxWidth().padding(16.dp)
-//
-//                        ) {
-//                            Text(text = it.description, fontSize = 20.sp)
-//                            Spacer(modifier = Modifier.height(4.dp))
-//                            Text(text = it.user.nickname, fontSize = 14.sp)
-//                        }
-//                    }
-//                }
-//            }
-//        }
-//    }
 }
